6737753fd48781c90789d3ea7550cbef3ec9edf9
Anton Korneyev <an.korn.zasekin@gmail.com>
2018-04-30 12:40:48 +0400

    initial commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StackNavigator } from 'react-navigation'
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>hello time!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
